5548de660b1aefbef45280cfd32f6ac0eb0ee94e
lollo110 <lorenzopaolocilurzo@gmail.com>
2025-08-22 16:38:47 +0200

    modif

--- assets/js/structure.js
+++ assets/js/structure.js
@@ -2,6 +2,7 @@ let motiv = document.querySelector('h1');
 let panter = document.querySelector('.pantera img');
 let fumo = document.querySelector('#position');
 let titre = document.querySelector('.title');
+let pantSdraiata = document.querySelector('.button img');
 
 window.addEventListener('load', function(){
 fumo.style.opacity = 1;
@@ -9,6 +10,7 @@ fumo.style.opacity = 1;
     setTimeout(() => {
     motiv.style.opacity = 1;
     titre.style.opacity = 1;
-       panter.style.opacity = 1;
+    panter.style.opacity = 1;
+    pantSdraiata.style.opacity = 1;
   }, 800);
 });
